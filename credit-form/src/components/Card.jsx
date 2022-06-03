20fcba4ef6115e864aaef8b0924e14f504da34ff
Aniket bansal <akbaisla29041998@gmail.com>
2022-06-03 15:38:51 +0530

    update

--- credit-form/src/components/Card.jsx
+++ credit-form/src/components/Card.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div></div>
+    <div>
+        <img src="https://www.visa.co.in/dam/VCOM/regional/ap/india/global-elements/images/in-visa-classic-card-498x280.png" />
+    </div>
   )
 }
 
--- credit-form/src/components/Form.jsx
+++ credit-form/src/components/Form.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import {FormControl, FormHelperText, FormLabel, Input} from "@chakra-ui/react"
+import {Button, FormControl, FormLabel, HStack, Input} from "@chakra-ui/react"
 const Form = () => {
   return (
-    <div>Payment Details
-    <FormControl>
-  <FormLabel>Cardholder Name</FormLabel>
-  <Input variant="flushed" type='number' placeholder="Enter youir name" />
- 
+    <div><h3 style={{fontWeight:"bold"}}>Payment Details</h3>
+    <FormControl space={5} style={{color:"grey"}}>
+  <FormLabel style={{fontWeight:"bold"}}>Cardholder Name</FormLabel>
+  <Input variant="flushed" type='text' placeholder="Enter your name" />
+  &nbsp;&nbsp;&nbsp;
+  <FormLabel style={{fontWeight:"bold"}}>Card Number</FormLabel>
+  <Input variant="flushed" type='number' placeholder="Enter your Card number here" />
+<br />
+<br />
+  <HStack spacing="20" >
+      <br />
+      <FormLabel style={{fontWeight:"bold"}}>Expiry month</FormLabel>
+      <FormLabel style={{fontWeight:"bold"}}>Expiry year</FormLabel>
+      <FormLabel style={{fontWeight:"bold"}}>CVV</FormLabel>
+  </HStack>
+  <HStack spacing="6">
+      <Input variant="flushed" type='number' size='xs'/>
+      <Input variant="flushed" type='number' size='xs'/>
+      <Input variant="flushed" type='number' size='xs'/>
+  </HStack>
+  <br/>
+<h2 style={{fontWeight:"bold"}}>Payment amount : 12 300 P</h2>
+
+<Button colorScheme='pink'>Submit</Button>
 </FormControl>
     </div>
   )
